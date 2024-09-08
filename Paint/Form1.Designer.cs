ad0d1108b4c202f950d97a96411abca7bc2c047b
Lygin04 <kosty.lygin.58@yandex.ru>
2024-09-08 19:10:35 +0300

    Добавьте файлы проекта.

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace AffineTransformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            canvas = new System.Windows.Forms.PictureBox();
+            drawButton = new System.Windows.Forms.Button();
+            scaleUpButton = new System.Windows.Forms.Button();
+            scaleDownButton = new System.Windows.Forms.Button();
+            reflectionButton = new System.Windows.Forms.Button();
+            clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)canvas).BeginInit();
+            SuspendLayout();
+            // 
+            // canvas
+            // 
+            canvas.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            canvas.BackColor = System.Drawing.SystemColors.Window;
+            canvas.Location = new System.Drawing.Point(7, 8);
+            canvas.Margin = new System.Windows.Forms.Padding(2);
+            canvas.Name = "canvas";
+            canvas.Size = new System.Drawing.Size(1180, 734);
+            canvas.TabIndex = 0;
+            canvas.TabStop = false;
+            canvas.MouseDown += canvas_MouseDown;
+            canvas.MouseMove += canvas_MouseMove;
+            canvas.MouseUp += canvas_MouseUp;
+            // 
+            // drawButton
+            // 
+            drawButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            drawButton.BackColor = System.Drawing.SystemColors.Info;
+            drawButton.Location = new System.Drawing.Point(7, 746);
+            drawButton.Margin = new System.Windows.Forms.Padding(2);
+            drawButton.Name = "drawButton";
+            drawButton.Size = new System.Drawing.Size(142, 29);
+            drawButton.TabIndex = 1;
+            drawButton.Text = "Нарисовать Боба";
+            drawButton.UseVisualStyleBackColor = false;
+            drawButton.Click += drawButton_Click;
+            // 
+            // scaleUpButton
+            // 
+            scaleUpButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            scaleUpButton.BackColor = System.Drawing.SystemColors.Info;
+            scaleUpButton.Location = new System.Drawing.Point(1191, 254);
+            scaleUpButton.Margin = new System.Windows.Forms.Padding(2);
+            scaleUpButton.Name = "scaleUpButton";
+            scaleUpButton.Size = new System.Drawing.Size(29, 42);
+            scaleUpButton.TabIndex = 3;
+            scaleUpButton.Text = "+";
+            scaleUpButton.UseVisualStyleBackColor = false;
+            scaleUpButton.Click += scaleUpButton_Click;
+            // 
+            // scaleDownButton
+            // 
+            scaleDownButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            scaleDownButton.BackColor = System.Drawing.SystemColors.Info;
+            scaleDownButton.Location = new System.Drawing.Point(1191, 309);
+            scaleDownButton.Margin = new System.Windows.Forms.Padding(2);
+            scaleDownButton.Name = "scaleDownButton";
+            scaleDownButton.Size = new System.Drawing.Size(29, 38);
+            scaleDownButton.TabIndex = 4;
+            scaleDownButton.Text = "-";
+            scaleDownButton.UseVisualStyleBackColor = false;
+            scaleDownButton.Click += scaleDownButton_Click;
+            // 
+            // reflectionButton
+            // 
+            reflectionButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            reflectionButton.BackColor = System.Drawing.SystemColors.Info;
+            reflectionButton.Location = new System.Drawing.Point(153, 746);
+            reflectionButton.Margin = new System.Windows.Forms.Padding(2);
+            reflectionButton.Name = "reflectionButton";
+            reflectionButton.Size = new System.Drawing.Size(142, 29);
+            reflectionButton.TabIndex = 5;
+            reflectionButton.Text = "Отражение";
+            reflectionButton.UseVisualStyleBackColor = false;
+            reflectionButton.Click += reflectionButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            clearButton.BackColor = System.Drawing.SystemColors.Info;
+            clearButton.Location = new System.Drawing.Point(299, 746);
+            clearButton.Margin = new System.Windows.Forms.Padding(2);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new System.Drawing.Size(142, 29);
+            clearButton.TabIndex = 7;
+            clearButton.Text = "Очистить";
+            clearButton.UseVisualStyleBackColor = false;
+            clearButton.Click += clearButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1220, 782);
+            Controls.Add(clearButton);
+            Controls.Add(reflectionButton);
+            Controls.Add(scaleDownButton);
+            Controls.Add(scaleUpButton);
+            Controls.Add(drawButton);
+            Controls.Add(canvas);
+            Margin = new System.Windows.Forms.Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)canvas).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Button scaleUpButton;
+        private System.Windows.Forms.Button scaleDownButton;
+        private System.Windows.Forms.Button reflectionButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AffineTransformation
+{
+    public partial class Form1 : Form
+    {
+        private string _logger = string.Empty;
+        private bool _isDragging;
+        private bool _isRotating;
+        private Point _lastMousePos;
+        private float _angle;
+
+        private float[,] _bob = 
+        {
+            {-4, 5, 1 },
+            { 4, 5, 1 },
+            { -4, -5, 1 },
+            { 4, -5, 1},
+            {4, -2, 1 },
+            {-4, -2, 1 },
+            {2, -7, 1},
+            {-2, -7, 1},
+            {2, -5, 1 },
+            {-2, -5, 1 },
+            {3, -7, 1},
+            {-3, -7, 1},
+            {-4, 1, 1},
+            {4, 1, 1},
+            {6, -2, 1},
+            {-6, -2, 1 },
+            {2, 3, 1 },
+            {2, 2, 1 },
+            {1, 2, 1 },
+            {1, 3, 1},
+            {-2, 3, 1 },
+            {-2, 2, 1 },
+            {-1, 2, 1 },
+            {-1, 3, 1},
+            {1, 0, 1},
+            {-1, 0, 1 },
+            {-1, 1, 1 },
+            {1, 1, 1}
+        };
+
+        private readonly int[,] _adjacent =
+        {
+            {1, 2 },
+            {2, 4 },
+            {4, 3 },
+            {3, 1 },
+            {13, 16 },
+            {14, 15 },
+            {10, 8 },
+            {8, 12 },
+            {9, 7 },
+            {7, 11 },
+            {6, 5 },
+            {26, 25 },
+            {25, 28 },
+            {28, 27 },
+            {27, 26 },
+            {21, 24 },
+            {24, 23 },
+            {23, 22 },
+            {22, 21 },
+            {20, 17 },
+            {17, 18 },
+            {18, 19 },
+            {19, 20}
+        };
+
+        // Туловище
+        private float[,] _body = 
+        {
+            { -4, 5, 1 },
+            { 4, 5, 1 },
+            { 4, -5, 1 },
+            { -4, -5, 1}
+        };
+
+        // Штаны
+        private float[,] _pants =
+        {
+            { 4, -2, 1 },
+            { -4, -2, 1 },
+        };
+
+        // Левая нога
+        private float[,] _leftLeg =
+        {
+            { -2, -5, 1 },
+            { -2, -7, 1 },
+            { -3, -7, 1 },
+            { -2, -7, 1 }
+        };
+
+        // Правая нога
+        private float[,] _rightLeg =
+        {
+            { 2, -5, 1 },
+            { 2, -7, 1 },
+            { 3, -7, 1 },
+            { 2, -7, 1 }
+        };
+
+        // Левая рука
+        private float[,] _leftHand =
+        {
+            { -4, 1, 1 },
+            { -6, -2, 1 }
+        };
+
+        // Правая рука
+        private float[,] _rightHand =
+        {
+            { 4, 1, 1 },
+            { 6, -2, 1 }
+        };
+
+        // Левый глаз
+        private float[,] _leftEye =
+        {
+            { -2, 3, 1 },
+            { -2, 2, 1 },
+            { -1, 2 , 1 },
+            { -1, 3, 1 }
+        };
+
+        // Правый глаз
+        private float[,] _rightEye =
+        {
+            { 2, 3, 1 },
+            { 2, 2, 1 },
+            { 1, 2 , 1 },
+            { 1, 3, 1 }
+        };
+
+        // Правый глаз
+        private float[,] _mouth =
+        {
+            { 1, 0, 1 },
+            { -1, 0, 1 },
+            { -1, 1 , 1 },
+            { 1, 1, 1 }
+        };
+
+        private readonly float[,] _reset;
+
+        // Уменьшение
+        private readonly float[,] _scaleDown =
+        {
+            { 0.5f, 0, 0 },
+            { 0, 0.5f, 0 },
+            { 0, 0, 1 }
+        };
+
+        // Увеличение
+        private readonly float[,] _scaleUp =
+        {
+            { 2, 0, 0 },
+            { 0, 2, 0 },
+            { 0, 0, 1 }
+        };
+
+        // Отражение
+        private readonly float[,] _reflect =
+        {
+            { 1, 0, 0 },
+            { 0, -1, 0 },
+            { 0, 0, 1 },
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas.Paint += PictureBox1_Paint;
+
+            _reset = _bob;
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int w = canvas.ClientSize.Width / 2;
+            int h = canvas.ClientSize.Height / 2;
+            e.Graphics.TranslateTransform(w, h);
+            DrawXAxis(new Point(-w, 0), new Point(w, 0), e.Graphics);
+            DrawYAxis(new Point(0, h), new Point(0, -h), e.Graphics);
+            e.Graphics.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+        }
+
+        private void DrawXAxis(Point start, Point end, Graphics g)
+        {
+            g.DrawLine(Pens.Black, start, end);
+            for (int i = start.X; i < end.X; i += 20)
+            {
+                g.DrawLine(Pens.LightGray, i, -1000, i, 1000);
+            }
+        }
+
+        private void DrawYAxis(Point start, Point end, Graphics g)
+        {
+            g.DrawLine(Pens.Black, start, end);
+            for (int i = start.Y; i > end.Y; i -= 20)
+            {
+                g.DrawLine(Pens.LightGray, -1000, i, 1000, i);
+            }
+        }
+
+        private void DrawBob()
+        {
+            DrawFigure(_body);
+            DrawFigure(_pants);
+            DrawFigure(_leftLeg);
+            DrawFigure(_rightLeg);
+            DrawFigure(_leftHand);
+            DrawFigure(_rightHand);
+            DrawFigure(_leftEye);
+            DrawFigure(_rightEye);
+            DrawFigure(_mouth);
+        }
+
+        private void DrawFigure(float[,] figure)
+        {
+            Graphics g = canvas.CreateGraphics();
+            Pen pen = new Pen(Color.Orange);
+            int w = canvas.ClientSize.Width / 2;
+            int h = canvas.ClientSize.Height / 2;
+            g.TranslateTransform(w, h);
+            for (int i = 0; i < figure.Length / 3; i++)
+            {
+                if (i == figure.Length / 3 - 1)
+                {
+                    g.DrawLine(pen, figure[i, 0] * 20, figure[i, 1] * 20,
+                        figure[0, 0] * 20, figure[0, 1] * 20);
+                }
+                else
+                {
+                    g.DrawLine(pen, figure[i, 0] * 20, figure[i, 1] * 20,
+                        figure[i + 1, 0] * 20, figure[i + 1, 1] * 20);
+                }
+            }
+
+            g.Dispose();
+        }
+
+        private void DrawFigure()
+        {
+            Graphics g = canvas.CreateGraphics();
+            Pen pen = new Pen(Color.Orange);
+            int w = canvas.ClientSize.Width / 2;
+            int h = canvas.ClientSize.Height / 2;
+            g.TranslateTransform(w, h);
+            string logger = string.Empty;
+            for (int i = 0; i < _adjacent.Length / 2 - 1; i++)
+            {
+                if (i == _adjacent.Length / 2 - 1)
+                {
+                    g.DrawLine(pen, _bob[_adjacent[i, 0] - 1, 0] * 20, _bob[_adjacent[i, 1] - 1, 1] * 20,
+                        _bob[0, _adjacent[i + 1, 0] - 1] * 20, _bob[0, _adjacent[i + 1, 1] - 1] * 20);
+                }
+                else
+                {
+                    g.DrawLine(pen, _bob[_adjacent[i, 0] - 1, 0] * 20, _bob[_adjacent[i, 0] - 1, 1] * 20,
+                        _bob[_adjacent[i, 1] - 1, 0] * 20, _bob[_adjacent[i, 1] - 1, 1] * 20);
+
+                    logger += $"X: {_bob[_adjacent[i, 0] - 1, 0]}, Y:{_bob[_adjacent[i, 0] - 1, 1]}  -  " +
+                              $"X:{_bob[_adjacent[i, 1] - 1, 0]}, Y:{_bob[_adjacent[i, 1] - 1, 1]}\n";
+                }
+            }
+
+            g.Dispose();
+
+        }
+
+        private void MultiplicationBob(float[,] b)
+        {
+            _body = Multiplication(_body, b);
+            _pants = Multiplication(_pants, b);
+            _leftLeg = Multiplication(_leftLeg, b);
+            _rightLeg = Multiplication(_rightLeg, b);
+            _leftHand = Multiplication(_leftHand, b);
+            _rightHand = Multiplication(_rightHand, b);
+            _leftEye = Multiplication(_leftEye, b);
+            _rightEye = Multiplication(_rightEye, b);
+            _mouth = Multiplication(_mouth, b);
+        }
+
+        static float[,] Multiplication(float[,] a, float[,] b)
+        {
+            float[,] result = new float[a.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        result[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            DrawFigure();
+        }
+
+        private void scaleDownButton_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            _bob = Multiplication(_bob, _scaleDown);
+            DrawFigure();
+        }
+
+        private void scaleUpButton_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            _bob = Multiplication(_bob, _scaleUp);
+            DrawFigure();
+        }
+
+        private void reflectionButton_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            _bob = Multiplication(_bob, _reflect);
+            DrawFigure();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            _bob = _reset;
+            DrawFigure();
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                _isRotating = true;
+                canvas.Cursor = Cursors.SizeWE;
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                _isDragging = true;
+                canvas.Cursor = Cursors.SizeAll;
+            }
+            _lastMousePos = e.Location;
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                _isRotating = false;
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                _isDragging = false;
+            }
+            canvas.Cursor = Cursors.Default;
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            int deltaX = e.X - _lastMousePos.X;
+            int deltaY = e.Y - _lastMousePos.Y;
+            
+            if (_isRotating)
+            {
+                _logger += $"{e.X} - {_lastMousePos.X} = {deltaX}";
+
+                _angle = deltaX * 0.05f;
+
+                float cos = MathF.Cos(_angle);
+                float sin = MathF.Sin(_angle);
+
+                float[,] rotate =
+                {
+                    { cos, -sin, 0 },
+                    { sin, cos, 0 },
+                    { 0, 0, 1 }
+                };
+
+                _bob = Multiplication(_bob, rotate);
+
+                Refresh();
+                DrawFigure();
+
+                _lastMousePos = e.Location;
+            }
+
+            if (_isDragging)
+            {
+                _logger += $"X:{deltaX}, Y:{deltaY}\n";
+
+                float[,] translateMatrix =
+                {
+                    { 1, 0, 0 },
+                    { 0, 1, 0 },
+                    { deltaX, deltaY, 1 }
+                };
+
+                _bob = Multiplication(_bob, translateMatrix);
+
+                Refresh();
+                DrawFigure();
+
+                _lastMousePos = e.Location;
+            }
+        }
+    }
+}
