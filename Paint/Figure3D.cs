dbc774f91dd987716ff6bcdaeff7886e5cf69111
Lygin04 <kosty.lygin.58@yandex.ru>
2024-11-12 22:09:47 +0300

    bag fix

--- Paint/Figure3D.cs
+++ Paint/Figure3D.cs
@@ -8,13 +8,33 @@ namespace Paint
 {
     public class Figure3D
     {
+        private List<float[,]> _cubs = [];
         private float[,] _figure;                         // Матрицы фигуры.
         private readonly int[,] _adjacent;                // Смежная матрица фигуры.
         private readonly float[,] _reset;                 // Исходная матрица фигуры.
         private readonly PictureBox _canvas;              // Холст на котором всё рисуется.
+        
+        private int[,] _indices =
+        {
+            {0, 1, 2}, // 0
+            {0, 2, 3}, // 1
 
-        private List<float> log = new List<float>();
+            {4, 6, 5}, // 2
+            {4, 7, 6}, // 3
 
+            {0, 5, 1}, // 4
+            {0, 4, 5}, // 5
+
+            {1, 5, 2}, // 6
+            {6, 2, 5}, // 7
+
+            {3, 2, 6}, // 8
+            {3, 6, 7}, // 9
+
+            {3, 4, 0}, // 10
+            {4, 3, 7}, // 11
+        };
+        
         // Уменьшение
         private readonly float[,] _scaleDown =
         {
@@ -73,6 +93,32 @@ namespace Paint
         }
 
         /// <summary>
+        /// Перевод из мировых в экранные координаты.
+        /// </summary>
+        /// <param name="cubs"></param>
+        private List<float[,]> WorldToScreen(List<float[,]> cubs)
+        {
+            List<float[,]> worldPosition = new();
+            foreach (var cub in cubs)
+            {
+                var temp = Utils.Copy(cub);
+                var screenCenterX = (float)_canvas.ClientSize.Width / 2;
+                var screenCenterY = (float)_canvas.ClientSize.Height / 2;
+
+                var scale = 10f;
+
+                for (var i = 0; i < cub.Length / 4; i++)
+                {
+                    temp[i, 0] = screenCenterX + temp[i, 0] * scale;
+                    temp[i, 1] = screenCenterY + temp[i, 1] * scale;
+                }   
+                worldPosition.Add(temp);
+            }
+
+            return worldPosition;
+        }
+
+        /// <summary>
         /// Отрисовка фигуры.
         /// </summary>
         public void DrawFigure()
@@ -81,7 +127,6 @@ namespace Paint
             var g = _canvas.CreateGraphics();
             var brush = new SolidBrush(Color.FromArgb(128, Color.Blue));
             var pen = new Pen(Color.Blue);
-            var d = _adjacent.GetLength(0);
 
             for (var i = 0; i < _adjacent.GetLength(0); i++)
             {
@@ -115,6 +160,51 @@ namespace Paint
             g.Dispose();
         }
 
+        /// <summary>
+        /// Отрисовка фигуры.
+        /// </summary>
+        public void DrawCubs()
+        {
+            var temps = WorldToScreen(_cubs);
+            var g = _canvas.CreateGraphics();
+            var brush = new SolidBrush(Color.FromArgb(128, Color.Blue));
+            var pen = new Pen(Color.Blue);
+
+            foreach (var temp in temps)
+            {
+                for (var i = 0; i < _indices.GetLength(0); i++)
+                {
+                    var v1 = _indices[i, 0];
+                    var v2 = _indices[i, 1];
+                    var v3 = _indices[i, 2];
+
+                    var V1 = new Vector3(temp[v1, 0], temp[v1, 1], temp[v1, 2]);
+                    var V2 = new Vector3(temp[v2, 0], temp[v2, 1], temp[v2, 2]);
+                    var V3 = new Vector3(temp[v3, 0], temp[v3, 1], temp[v3, 2]);
+
+                    var t1 = V1 - V2;
+                    var t2 = V2 - V3;
+
+                    var normal = Vector3.Normalize(Vector3.Cross(t1, t2));
+                    var camera = new Vector3(0f, 0f, 1f);
+                    if (Vector3.Dot(camera, normal) > 0)
+                    {
+                        PointF[] points =
+                        {
+                            new(temp[v1, 0], temp[v1, 1]),
+                            new(temp[v2, 0], temp[v2, 1]),
+                            new(temp[v3, 0], temp[v3, 1])
+                        };
+
+                        //g.FillPolygon(brush, points);  // Закраска грани
+                        g.DrawPolygon(pen, points); // Обводка грани
+                    }
+                }
+
+                g.Dispose();
+            }
+        }
+        
         /// <summary>
         /// Умножение 2 матриц.
         /// </summary>
@@ -192,7 +282,6 @@ namespace Paint
             float cos = MathF.Cos(angle);
             float sin = MathF.Sin(angle);
 
-            log.Add(angle);
             float[,] rotateY =
             {
                 { cos, 0, sin, 0 },
@@ -238,8 +327,24 @@ namespace Paint
                 { 0, 0, 1, 0 },
                 { deltaX, deltaY, deltaZ, 1 }
             };
-            log.Add(deltaX);
             _figure = Multiplication(_figure, translateMatrix);
+        }
+
+        public void CreateCube(Point3D startPosition, float sizeX, float sizeY, float sizeZ)
+        {
+            float[,] cube =
+            {
+                {startPosition.X, startPosition.Y, startPosition.Z, 1},
+                {startPosition.X - sizeX, startPosition.Y, startPosition.Z, 1},
+                {startPosition.X - sizeX, startPosition.Y, startPosition.Z + sizeZ, 1},
+                {startPosition.X, startPosition.Y, startPosition.Z + sizeZ, 1},
+                
+                {startPosition.X, startPosition.Y - sizeY, startPosition.Z, 1},
+                {startPosition.X + sizeX, startPosition.Y - sizeY, startPosition.Z, 1},
+                {startPosition.X - sizeX, startPosition.Y - sizeY, startPosition.Z + sizeZ, 1},
+                {startPosition.X, startPosition.Y - sizeY, startPosition.Z + sizeZ, 1},
+            };
+            _cubs.Add(cube);
         }
     }
 }
---
+++ Paint/Point3D.cs
@@ -0,0 +1,8 @@
+namespace Paint;
+
+public class Point3D(float x, float y, float z)
+{
+    public float X { get; set; } = x;
+    public float Y { get; set; } = y;
+    public float Z { get; set; } = z;
+}
